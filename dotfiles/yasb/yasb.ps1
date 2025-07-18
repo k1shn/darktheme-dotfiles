9ed107c13542572079bbea53ae1963f2534f28f4
k1shqn <actkiyoshi@gmail.com>
2025-07-17 18:51:30 -0700

    main

--- dotfiles/yasb/yasb.ps1
+++
@@ -1,6 +0,0 @@
-# yasb.ps1
-
-$source = "$env:USERPROFILE\\Pictures\\yasb\\current.jpg"  # Ganti sesuai path YASB lo
-$target = "$env:USERPROFILE\\yasb\\anim\\wallpaper.jpg"
-
-Copy-Item $source $target -Force
